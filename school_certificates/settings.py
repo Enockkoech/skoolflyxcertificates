46cdb17f09f5961ccf9632b5246ad91940642ebd
babafrank <chumbacha@gmail.com>
2024-07-20 15:39:43 +0300

    updated .env

--- school_certificates/settings.py
+++ school_certificates/settings.py
@@ -90,11 +90,11 @@ WSGI_APPLICATION = 'school_certificates.wsgi.application'
 #     }
 # }
 
-# database_url = os.environ.get("DATABASE_URL")
+# DATABASE_URL = os.environ.get("DATABASE_URL")
 
-# DATABASES["default"] = dj_database_url.parse("database_url")
+# DATABASES["default"] = dj_database_url.parse("DATABASE_URL")
 DATABASES = {
-    'default': dj_database_url.config(default ='postgresql://school_certificates_db_user:atRrh77Oifk1iELN0ENmlPzRMDySl9YA@dpg-cqdnnm0gph6c73a9lf00-a/school_certificates_db')
+    'default': dj_database_url.config(default ='postgresql://school_certificates_db_user:atRrh77Oifk1iELN0ENmlPzRMDySl9YA@skoolflyxcertificates.onrender.com/school_certificates_db')
 }
 
 # Password validation
