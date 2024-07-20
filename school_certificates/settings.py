9a26f7367f3c35875e278d26fc3c66dac63f2aae
babafrank <chumbacha@gmail.com>
2024-07-20 12:16:00 +0300

    updated recquirements.txt

--- school_certificates/settings.py
+++ school_certificates/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y6(m_0ns+jnm3st*u^&ro$ejxy3rwwn@=bwwon2d7mtm18t@q!'
+# SECRET_KEY = 'django-insecure-y6(m_0ns+jnm3st*u^&ro$ejxy3rwwn@=bwwon2d7mtm18t@q!'
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
